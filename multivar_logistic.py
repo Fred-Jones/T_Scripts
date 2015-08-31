86abbc9a476000541afaef73c4e7d84cec348ec2
aypapi <mmaammbbuu@gmail.com>
2015-08-31 08:37:30 -0600

    added multivariate

---
+++ multivar_logistic.py
@@ -0,0 +1,22 @@
+import numpy as np
+from theano import *
+import theano.tensor as T
+from theano import function
+
+x = T.dmatrix('x')
+theta = T.dmatrix('theta')
+y = T.dmatrix('y')
+
+z = 1/(1+T.exp(-T.dot(theta.T,x)))
+f = function([x,theta], z)
+
+xtest = np.arange(1,11,1)
+xtest.shape = (2,5)
+
+thetatest = np.arange(0.1,1.1,.1)
+thetatest.shape = (2,len(thetatest)/2)
+
+print f(xtest, thetatest)
+print '---'
+print  thetatest
+print xtest
