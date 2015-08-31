3d3c918281513455d90330730d439537c7d05431
aypapi <mmaammbbuu@gmail.com>
2015-08-31 10:06:54 -0600

    added vec/mat implementations of forward prop

--- forward_prop_vec.py
+++ forward_prop_vec.py
@@ -9,6 +9,7 @@ theta = T.vector('theta')
 y = T.dmatrix('y')
 
 z = T.dot(theta.T,X)
+#test z = transpose(theta) * X
 g = function([X, theta], z)
 
 XX = np.arange(1,11,1)
@@ -17,10 +18,8 @@ thetaxx = np.arange(0.1,.6,.05)
 zz = g(XX,thetaxx)
 print 'theta.T * X = ',zz
 
-##compute activation
-# dprod = T.scalar('dprod')
+##compute activation given input vector=XX and weights=thetaxx
 gee = 1/(1+T.exp(-z))
 f = function([X, theta], gee)
-# fb = function([z], gee)
 a = f(XX,thetaxx)
 print 'Activationof layer two = g(z2) = a2 =', a
